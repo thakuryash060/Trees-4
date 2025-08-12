3d8230d4f65602f142c592ca199231028a0371c9
Yash Thakur <144375330+thakuryash060@users.noreply.github.com>
2025-08-12 16:02:19 +0530

    Create Q2.java
    Time Complexity : O(n)
    Space Complexity : O(1)

---
+++ Q2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null)
+        {
+            return null;
+        }
+
+        while(true){
+            if(root.val < p.val && root.val < q.val)
+            {
+                root = root.right;
+            }
+
+            else if(root.val > p.val && root.val > q.val)
+            {
+                root = root.left;
+            }
+
+            else
+            {
+                return root;
+            }
+        }
+        
+    }
+}
